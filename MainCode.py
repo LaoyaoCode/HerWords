526e0bb01f34378aa9bd0bc51b0cb092df1df420
LaoyaoCode <LaoyaoCode@outlook.com>
2018-03-25 10:26:53 +0800

    添加了保存到文件的功能

--- MainCode.py
+++ MainCode.py
@@ -6,11 +6,19 @@ from itchat.content import *
 from SQLControler import SQLController
 from SQLControler import WordsModel
 
+#保存她的文字
+def SaveWordsToTxt(words):
+    file = open("HerWords.txt", 'a+', encoding='utf-8')
+    file.write(words + '\r\n')
+    file.close()
 
 @itchat.msg_register(TEXT)
 def others_send_me_message(msg):
+
     #是她发给我的消息
     if(Friend['UserName'] == msg['FromUserName']):
+        #保存数据到文件中
+        SaveWordsToTxt(msg['Content'])
         print('She Send Me : ' + msg['Content'])
         model = WordsModel(msg['Content'])
         Controller.AddWords((model))
@@ -27,4 +35,5 @@ print('登陆成功')
 Friend = itchat.search_friends(name='何姗')[0]
 print("获得她的信息 : " + Friend['NickName'])
 itchat.run(True)
+
 
